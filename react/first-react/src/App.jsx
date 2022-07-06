d953294ff639a6d49d3490bbbd93834c7b181d24
Sean McNeil <smcneil136@gmail.com>
2022-07-06 16:45:58 -0400

    MERN Week 1, Day 3 | Afternoon Lecture (React)

---
+++ react/first-react/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <table>
+        <tr>
+          <th>ksjhdfk</th>
+          <th>kjahfkfk</th>
+        </tr>
+        <tr>
+          <td>akdfjs</td>
+          <td>asjks</td>
+        </tr>
+      </table>
+      <h4>This is a new element below the table</h4>
+    </div>
+  );
+}
+
+export default App;
