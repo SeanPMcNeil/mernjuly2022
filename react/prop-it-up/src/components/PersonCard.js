51d7894ad4587868e487083656bac18d6663738a
Sean McNeil <smcneil136@gmail.com>
2022-07-06 18:43:35 -0400

    updates for "Putting It Together" assignment

--- react/prop-it-up/src/components/PersonCard.js
+++ react/prop-it-up/src/components/PersonCard.js
@@ -1,15 +1,25 @@
-import React, { Component } from "react";
+import { Component } from "react";
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicks: props.age
+        };
+    }
+    
     render() {
         return (
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: { this.state.clicks }</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={ this.happyBirthday }>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         )
     }
+
+    happyBirthday = () => this.setState((state) => ({ clicks: state.clicks + 1 }));
 }
 
 export default Person;
